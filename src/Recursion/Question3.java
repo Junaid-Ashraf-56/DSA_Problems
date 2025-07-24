072befc5d6d73a35eefc144676fe991c59662a02
Junaid-Ashraf-56 <ashrafjunaid848@gmail.com>
2025-07-24 22:16:15 +0500

    Recusion

--- src/Recursion/Question3.java
+++ src/Recursion/Question3.java
@@ -2,6 +2,7 @@ package Recursion;
 
 //Question 4: Sum of n Numbers
 // Question 4.1 :Sum of digits
+//Question 4.2 : Product of digits
 public class Question3 {
     public static int Sum(int n){
         if (n==1){
@@ -17,8 +18,15 @@ public class Question3 {
         int rem = n%10;
         return  rem + sumOfDigits (n/10);
     }
+    public static int productOfDigits(int n){
+        if (n<1){
+            return 0;
+        }
+        return  (n%10) * sumOfDigits (n/10);
+    }
     public static void main(String[] args) {
         System.out.println(Sum(5));
-          System.out.println(sumOfDigits(1432));
+        System.out.println(sumOfDigits(1432));
+        System.out.println(productOfDigits(5));
     }
 }
