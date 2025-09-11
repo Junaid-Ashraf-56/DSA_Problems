2091745feaef04fc200418c9c64407c0328b492e
Junaid-Ashraf-56 <ashrafjunaid848@gmail.com>
2025-09-12 00:58:12 +0500

    Recursion

---
+++ src/Recursion/Permutations/Question1.java
@@ -0,0 +1,8 @@
+package Recursion.Permutations;
+
+public class Question1 {
+    //Question 1: I have to know that how many permutation can be made for making of a digit after throwing dices?
+    public static void main(String[] args) {
+
+    }
+}
